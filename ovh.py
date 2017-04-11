7743fcfb7cd10597827fc13a3937aec7dab7b1d0
dbellamy <damien.bellamy@arkena.com>
2017-04-11 14:55:17 +0200

    Added reboot function, fixed check exit state

--- ovh.py
+++ ovh.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""Module ansible pour reinstall / reboot / stop / start les vps ovh."""
 
 import hashlib
 import json
@@ -33,29 +34,22 @@ ssh_keys = module.params['ssh_keys']
 # On récupère la liste des SSH keys au format adéquat
 ssh_keys = json.dumps(ssh_keys)
 
-# On récupère la date à de suite maintenant
-now = str(int(time.time()))
+# On set body à rien au cas où il n'est pas utilisé (ce champ est optionnel)
+body = ""
 
-# On déclare le body et la signature
-body = ''
-signature = ''
 
-# On déclare les headers simples pour la requête api
-headers = {
-    'X-Ovh-Application': "%s" % (appkey),
-    'X-Ovh-Timestamp': "%s" % (now),
-    'X-Ovh-Consumer': "%s" % (consumerkey)
-}
-
-# On déclare le type de body
-headers['Content-type'] = 'application/json'
-
-# On entre dans la boucle si l'action demandée est reinstall
-if "reinstall" in action:
-    # Méthode GET pour récupérer l'id de la distribution à réinstaller
-    method = "GET"
-    target = "https://eu.api.ovh.com/1.0/vps/%s/distribution" % (service)
-    # A transformer en fonction, join de la signature et hash
+def generate_request(body, appsecret, consumerkey, target, method):
+    u"""Génère la requête complète avec signature."""
+    # On récupère la date à de suite maintenant
+    now = str(int(time.time()))
+    # On déclare les headers simples pour la requête api
+    headers = {
+        'X-Ovh-Application': "%s" % (appkey),
+        'X-Ovh-Timestamp': "%s" % (now),
+        'X-Ovh-Consumer': "%s" % (consumerkey),
+        'Content-type': "application/json"
+    }
+    # On génère la signature
     signature = "+".join([
         appsecret, consumerkey,
         method, target,
@@ -68,7 +62,23 @@ if "reinstall" in action:
     # comme il faut
     headers['X-Ovh-Signature'] = "$1$" + signature.hexdigest()
     # On exec la requête et on la store dans r
-    r = requests.get(target, headers=headers, data=body)
+    if method == "GET":
+        return requests.get(target, headers=headers, data=body)
+    if method == "POST":
+        return requests.post(target, headers=headers, data=body)
+    if method == "DELETE":
+        return requests.delete(target, headers=headers, data=body)
+    if method == "PUT":
+        return requests.put(target, headers=headers, data=body)
+    return None
+
+# On entre dans la boucle si l'action demandée est reinstall
+if action == "reinstall":
+    # Méthode GET pour récupérer l'id de la distribution à réinstaller
+    method = "GET"
+    target = "https://eu.api.ovh.com/1.0/vps/%s/distribution" % (service)
+    # On génère la requête
+    r = generate_request(body, appsecret, consumerkey, target, method)
     # On dump la réponse au format json et on la store dans out
     out = json.loads(r.text)
     # On récupère le template_id
@@ -81,24 +91,26 @@ if "reinstall" in action:
         template_id,
         ssh_keys
     )
-    # Ceci sera VRAIMENT à transformer en fonction
-    signature = "+".join([
-        appsecret, consumerkey,
-        method, target,
-        body,
-        now
-    ]).encode('utf-8')
-    # On hash la signature au format sha1
-    signature = hashlib.sha1(signature)
-    # On déclare le header pour la signature en terminant de la formater
-    # comme il faut
-    headers['X-Ovh-Signature'] = "$1$" + signature.hexdigest()
-    # On exécute la requête de reinstall
-    r = requests.post(target, headers=headers, data=body)
+    # On génère la requête
+    r = generate_request(body, appsecret, consumerkey, target, method)
     # On récupère la réponse
     out = json.loads(r.text)
-    # On a une réponse "message" si quelque chose ne marche pas
-    if "message" in out:
+    try:
+        # On récupère le contenu du champ state
+        state = out["state"]
+        # todo est la valeur attendue pour le champ state, on traite le cas et
+        # on renvoie à Ansible un json indiquant que le changement a eu lieu
+        # + code retour ok
+        if state == "todo":
+            print json.dumps({
+                "changed": True,
+                "action": "%s" % action
+            })
+            sys.exit(0)
+    # Si le service est déjà à l'état attendu ou en cas de souci,
+    # le json renvoyé n'a pas de champ "state" mais un champ "message".
+    # On traite le cas de figure
+    except KeyError:
         msg = out["message"]
     # On renvoie un json à ansible indiquant qu'il n'y a eu aucun changement si
     # on détecte dans la réponse que le reinstall n'a pas été fait car il y a
@@ -110,17 +122,6 @@ if "reinstall" in action:
                 "action": "Too many actions already occuring"
             })
             sys.exit(1)
-    # On récupère le contenu du champ state
-    state = out["state"]
-    # todo est la valeur attendue pour le champ state, on traite le cas et
-    # on renvoie à Ansible un json indiquant que le changement a eu lieu
-    # + code retour ok
-    if state == "todo":
-        print json.dumps({
-            "changed": True,
-            "action": "%s" % action
-        })
-    sys.exit(0)
     # On récupère le code retour de la requête api, s'il est différent de 200
     # on renvoie un json à ansible indiquant que l'action a échoué
     # + code retour erreur
@@ -129,42 +130,38 @@ if "reinstall" in action:
             "failed": True,
             "msg": "Failed"
         })
-    sys.exit(1)
+        sys.exit(1)
 
 # On entre dnas la boucle suivante si les actions demandées
-# sont start ou stop
-if action in ['start','stop']:
+# sont reboot ou stop
+if action in ['start', 'reboot', 'stop']:
     # On prépare la requête
     target = "https://eu.api.ovh.com/1.0/vps/%s/%s" % (service, action)
     method = "POST"
-    # Ma parole va falloir faire une fonction...
-    signature = "+".join([
-        appsecret, consumerkey,
-        method, target,
-        body,
-        now
-    ]).encode('utf-8')
-    # On hash la signature au format sha1
-    signature = hashlib.sha1(signature)
-    # On déclare le header pour la signature en terminant de la formater
-    # comme il faut
-    headers['X-Ovh-Signature'] = "$1$" + signature.hexdigest()
-    # On exécute la requête API et on store le résultat dans la variable r
-    r = requests.post(target, headers=headers, data=body)
-
+    # On génère la requete
+    r = generate_request(body, appsecret, consumerkey, target, method)
     # On charge la réponse json dans la variable out. A savoir, le r.json
     # ne donne pas le résultat escompté pour la suite,
     # bug avec if "bidule" in out
     out = json.loads(r.text)
 
+    try:
+        # On récupère le contenu du champ state
+        state = out["state"]
+        # todo est la valeur attendue pour le champ state, on traite le cas et
+        # on renvoie à Ansible un json indiquant que le changement a eu lieu
+        # + code retour ok
+        if state == "todo":
+            print json.dumps({
+                "changed": True,
+                "action": "%s" % action
+            })
+            sys.exit(0)
     # Si le service est déjà à l'état attendu ou en cas de souci,
     # le json renvoyé n'a pas de champ "state" mais un champ "message".
     # On traite le cas de figure
-    if "message" in out:
+    except KeyError:
         msg = out["message"]
-    # On renvoie un json à ansible indiquant que l'action a échoué si
-    # on détecte dans la réponse que le service n'existe pas
-    # + code retour erreur
         if "exist" in msg:
             print json.dumps({
                 "failed": True,
@@ -180,17 +177,6 @@ if action in ['start','stop']:
                 "action": "%s" % action
             })
             sys.exit(0)
-    # On récupère le contenu du champ state
-    state = out["state"]
-    # todo est la valeur attendue pour le champ state, on traite le cas et
-    # on renvoie à Ansible un json indiquant que le changement a eu lieu
-    # + code retour ok
-    if state == "todo":
-        print json.dumps({
-            "changed": True,
-            "action": "%s" % action
-        })
-    sys.exit(0)
     # On récupère le code retour de la requête api, s'il est différent de 200
     # on renvoie un json à ansible indiquant que l'action a échoué
     # + code retour erreur
@@ -199,4 +185,4 @@ if action in ['start','stop']:
             "failed": True,
             "msg": "Failed"
         })
-    sys.exit(1)
+        sys.exit(1)
